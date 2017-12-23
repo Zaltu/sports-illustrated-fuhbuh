ab5f1dfafa67de119dcf566eba0a20d784f0b088
Zaltu <samuel.waugh@autodesk.com>
2017-12-23 13:26:53 -0500

    Real skeleton

---
+++ libs/json_reader.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import json
+
+
+def buildPath(fileName):
+    """
+    Standardizes filepath to conform with an executable version built using pyinstaller
+
+    :param string filename: name of the file
+    :return: state-dependant filepath
+    :rtype: string
+    """
+    if getattr(sys, 'frozen', False):
+        return os.path.join(os.path.dirname(sys.executable), str(fileName))
+    return fileName
+
+
+def writeJson(filename, data):
+    with open(buildPath(filename), 'w') as outfile:
+        json.dump(data, outfile, default=lambda o: o.__dict__, sort_keys=True)
+    return 0
+
+
+def readJson(filename):
+    try:
+        with open(buildPath(filename)) as json_data:
+            array = json.load(json_data)
+        return array
+    except IOError as ioerror:
+        print ioerror
+        return 1
