d527853a7cbc17138b80e8d908b0498efbce8ed6
Zaltu <samuel.waugh@autodesk.com>
2017-12-26 19:34:51 -0500

    Adding priority part 2 (in progress)

---
+++ src/play/play.py
@@ -0,0 +1,25 @@
+from libs import json_reader
+import src.GAME as GAME
+import math.random
+
+
+#TODO validate ecessity of 'stance' attribute
+def roll(callout, stance): # eg roll('Line Plunge', 'offense')
+    stats = json_reader.read(GAME.team, attribute=stance)
+    roll = math.random()
+    result = stats[callout][GAME.weightedRoll(callout, stance, roll)]
+    return result
+    #host = conn.socket.validateResolveHost TODO: this goes to dispatcher
+
+
+def processPlay(rolls):
+	finalType = determinePriority(rolls['Defense'], rolls['Offense'])
+
+def determinePriority(defRoll, attRoll):
+	defType = _rolltype(defRoll)
+	if defType not in GAME.simplePriorityLow:
+		return defRoll
+	elif attRoll not in GAME.simplePriorityLow
+
+def _rolltype(roll):
+	return roll.split(" ")[0]
---
+++ src/turnController.py
@@ -0,0 +1,10 @@
+from gui.liaison import notify
+from src import GAME
+
+def graphicDispatch(play)
+    playToGame(play)
+    notify()
+
+
+def playToGame(play)
+    GAME.play = play
